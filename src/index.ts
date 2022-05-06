33eeb90e1e17ed720d669531cb6d23582bf1f967
Sam <samuv.eth@gmail.com>
2022-05-06 16:12:02 +0700

    Fixes

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { getInstance } from "./LockPlugin";
+import LockPlugin from "./LockPlugin";
+import { getInjectedWallet } from "./utils";
+
+export { LockPlugin, getInstance, getInjectedWallet };
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,35 @@ import typescript from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    typescript({
+      check: false,
+      include: ["./src/**/*.ts"],
+      exclude: ["vite.config.ts"],
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true,
+          declarationMap: true,
+        },
+      },
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
